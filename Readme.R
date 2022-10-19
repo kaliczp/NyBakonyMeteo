c45cf8325b6e5c025973b1d977ff6e78c8bdc9d1
Péter Kalicz <kaliczp@gmail.com>
2022-10-19 16:39:43 +0200

    Idősor előállítás, adathiány kezelés

--- Readme.R
+++ Readme.R
@@ -1,6 +1,6 @@
 # Állomáskód beolvasás
 files <- dir(pattern = "csv")
-rawdata <- read.table(files[1], sep = ";", skip =5, head = TRUE)
+rawdata <- read.table(files[1], sep = ";", skip =5, head = TRUE, na.strings = " -999")
 # Első dátum lekérdezése
 as.character(head(rawdata$Time))
 tail(rawdata[,"Time"])
@@ -8,3 +8,10 @@ as.character(rawdata[1:5,"Time"])
 
 # Időbélyeg
 ido <- as.POSIXct(strptime(rawdata$Time, "%Y%m%d%H%M"))
+
+library(xts)
+
+Prec <- xts(rawdata$r, order.by = ido)
+apply.yearly(Prec, sum)
+
+Precall <- Prec
