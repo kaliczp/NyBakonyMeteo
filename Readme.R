0442127f77066a1871b0e2ffad95693edbeab5a8
Péter Kalicz <kaliczp@gmail.com>
2022-10-19 16:23:38 +0200

    Időbélyeg tesztelés és előállítás

--- Readme.R
+++ Readme.R
@@ -1,3 +1,10 @@
 # Állomáskód beolvasás
 files <- dir(pattern = "csv")
 rawdata <- read.table(files[1], sep = ";", skip =5, head = TRUE)
+# Első dátum lekérdezése
+as.character(head(rawdata$Time))
+tail(rawdata[,"Time"])
+as.character(rawdata[1:5,"Time"])
+
+# Időbélyeg
+ido <- as.POSIXct(strptime(rawdata$Time, "%Y%m%d%H%M"))
